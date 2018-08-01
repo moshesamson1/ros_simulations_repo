daf8329789d6a549662f60abecc4e2adb9ee67ff
Samson Moshe <samson.moshe@gmail.com>
2018-08-01 19:40:42 +0300

    managed to move two robot independently

--- src/coverage_node.py
+++ src/coverage_node.py
@@ -6,32 +6,24 @@
 #      Author: Moshe Samson
 #
 
-import rospy, sys, math
-from nav_msgs.srv import GetMap
-from nav_msgs.msg import OccupancyGrid
+import math
+import sys
 from random import shuffle
-import tf
-import pickle
-import Entities
+import numpy as np
+
+import rospy
 from geometry_msgs.msg import Twist
-import actionlib
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from tf.transformations import quaternion_from_euler
-from geometry_msgs.msg import Quaternion
-from math import pi
+from nav_msgs.srv import GetMap
+from turtlesim.msg import Pose
+
+import Entities
 
 
-def print_map_to_file():
-    with open("new_grid.txt", "w+") as coarse_grid_file:
-        global coarse_grid
-        for row in reversed(coarse_grid):
-            for cell in row:
-                coarse_grid_file.write("1") if cell else coarse_grid_file.write("0")
-            coarse_grid_file.write("\n")
-    rospy.loginfo("finished writing coarse_grid.txt")
+def pose_callback(pose_msg):
+    # Do nothing
+    return
 
-
-def creat_occupancy_grid(my_map):
+def create_occupancy_grid(my_map):
     # creating the occupancy grid
     global grid
     grid = [[None] * my_map.info.width for i in xrange(my_map.info.height)]
@@ -45,7 +37,7 @@ def creat_occupancy_grid(my_map):
 
 
 def create_occupancy_grid_using_robot_size(my_map, robot_size):
-    rospy.loginfo("creat_occupancy_grid_using_d")
+    rospy.loginfo("create_occupancy_grid_using_d")
 
     robot_radius = robot_size
     robot_map_size = int(math.ceil(robot_radius / my_map.info.resolution))
@@ -122,7 +114,8 @@ def bfs(graph, start):
     while queue:
         vertex = queue.pop(0)
         if vertex not in visited:
-            if vertex not in visited: visited.append(vertex)
+            if vertex not in visited:
+                visited.append(vertex)
             queue.extend(graph[vertex] - set(visited))
     return visited
 
@@ -187,18 +180,18 @@ def mst(start, _graph):
     return edges
 
 
-def PrintGraph(edges):
+def print_graph(edges):
     import matplotlib.pyplot as plt
     import networkx as nx
     plt.figure()
-    G = nx.DiGraph()
-    G.add_edges_from(edges)
+    g = nx.DiGraph()
+    g.add_edges_from(edges)
     positions = {}
     color_map = []
-    for n in G.nodes():
+    for n in g.nodes():
         positions[n] = (n[0], n[1])
         color_map.append('green' if n == edges[0][0] else 'red')
-    nx.draw(G, pos=positions, node_size=60, font_size=8, node_color=color_map)
+    nx.draw(g, pos=positions, node_size=60, font_size=8, node_color=color_map)
     plt.show()
 
 
@@ -362,100 +355,52 @@ def get_coverage_path_from_mst(mst, start):
     # PrintGraph(itemlist)
 
 
-def move_robot(prev_cell, cell, should_rotate_ang):
-    global command_pub, robot_size
+def move_blindly(last_d, new_d, distance):
+    global robot_name, pub, sub
+    print "Moving " + new_d + "...",
 
-    # Setting the current time for distance calculus
-    t0 = rospy.Time.now().to_sec()
-    current_distance = 0
-    move_msg = Twist()
-    move_msg.linear.x = 0.5
-    angle = math.radians(should_rotate_ang)  # 45 deg in radiands
+    rotation_angle_deg = 0
+    if (last_d == 'W' and new_d == 'N') or (last_d == 'N' and new_d == 'E') or \
+            (last_d == 'E' and new_d == 'S') or (last_d == 'S' and new_d == 'W'):
+        rotation_angle_deg = 90
+    elif (last_d == 'W' and new_d == 'S') or (last_d == 'S' and new_d == 'E') or \
+            (last_d == 'E' and new_d == 'N') or (last_d == 'N' and new_d == 'W'):
+        rotation_angle_deg = -90
+    else:
+        rotation_angle_deg = 0
+
+    angle = np.deg2rad(rotation_angle_deg)
+    angle_vel = angle * 0.5
+
+    move_forward_msg = Twist()
+    move_forward_msg.linear.x = 0.5  # speed
     rotate_msg = Twist()
-    rotate_msg.angular.z = angle * 0.5;
+    rotate_msg.angular.z = angle_vel
+    stay_put_msg = Twist()
 
-    print prev_cell
-    print cell
-    print move_msg
-    print " "
-
-    # setting the current time for angle calculations
     t0 = rospy.Time.now().to_sec()
-    current_angle = 0
-
-    # loop to rotate the turtle a specified amount of degrees
-    while current_angle < math.fabs(angle):
-        # publish angular velocity
-        command_pub.publish(rotate_msg)
-        # take the actual time
+    current_angle = 0.0
+    while current_angle < angle:
+        pub.publish(rotate_msg)
         t1 = rospy.Time.now().to_sec()
-        # calcuate current angle, using elapsed time and given angular speed
-        current_angle = 0.5 * angle * (t1 - t0)
+        current_angle = angle_vel * (t1 - t0)
+    pub.publish(stay_put_msg)
 
-    # after the loop, stop the robot from turning anymore
-    command_pub.publish(Twist())
-    rospy.sleep(1)
-
-    # loop to move the turtle a specified distance
+    current_distance = 0.0
     t0 = rospy.Time.now().to_sec()
-    while current_distance < robot_size:
-        # publish velocity
-        command_pub.publish(move_msg)
-        # take the actual time
+    while current_distance < distance:
+        pub.publish(move_forward_msg)
         t1 = rospy.Time.now().to_sec()
-        # calculate current distance using elapsed time and given speed
         current_distance = 0.5 * (t1 - t0)
 
-    # after the loop, stop the robot from moving forward anymore
-    command_pub.publish(Twist())
-    rospy.sleep(1)
+    pub.publish(stay_put_msg)
 
-
-def send_goals(x, y):
-    ac = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-    rospy.loginfo("Waiting for move_base action server...")
-
-    # Wait 60 seconds for the action server to become available
-    res = ac.wait_for_server(rospy.Duration(20))
-
-
-    rospy.loginfo("Connected to move base server")
-
-    # send a goal to move_base
-    goal = MoveBaseGoal()
-    # Use the map frame to define goal poses
-    goal.target_pose.header.frame_id = 'map'
-    # Set the time stamp to "now"
-    goal.target_pose.header.stamp = rospy.Time.now()
-    # Set x,y positions
-    goal.target_pose.pose.position.x = x  # rospy.get_param('goal_x')
-    goal.target_pose.pose.position.y = y  # rospy.get_param('goal_y')
-
-    # Convert the Euler angle to quaternion
-    theta_rad = 0.0  # rospy.get_param('goal_theta') * (pi / 180)
-    quat = quaternion_from_euler(0, 0, theta_rad)
-    q_msg = Quaternion(*quat)
-    goal.target_pose.pose.orientation = q_msg
-
-    # Sending the goal and waiting for the action to return
-    rospy.loginfo("Sending robot to: x = %f, y = %f, theta = %f" % (
-        goal.target_pose.pose.position.x, goal.target_pose.pose.position.y, 0.0))
-
-    ac.send_goal(goal)
-    ac.wait_for_result()
-
-    # Check for result status
-    if ac.get_state() == actionlib.GoalStatus.SUCCEEDED:
-        rospy.loginfo("You have reached the goal!")
-        return True
-    else:
-        rospy.loginfo("The base failed for some reason")
-        return False
+    print "Done."
 
 
 def main():
     rospy.init_node('coverage_node', argv=sys.argv)
-    global grid, coarse_grid, robot_size
+    global grid, coarse_grid, robot_size, robot_name, pub, sub
     starting_location = (0, 0)
     robot_size = 0.35  # meters!
     robot_name = ''
@@ -468,6 +413,12 @@ def main():
         robot_name = rospy.get_param('~robot_name')
         print "robot_name: {0}".format(robot_name)
 
+    # Publisher - context is inside namespace
+    pub = rospy.Publisher("mobile_base/commands/velocity", Twist, queue_size=10)
+
+
+    # Listener for pose
+    sub = rospy.Subscriber(robot_name + "/pose", Pose, pose_callback)
 
     # initiate map service
     rospy.wait_for_service('static_map')
@@ -481,22 +432,27 @@ def main():
         create_occupancy_grid_using_robot_size(response.map, robot_size)
 
         # find robot's position
-        listener = tf.TransformListener()
-        listener.waitForTransform('/map', robot_name + '/base_footprint', rospy.Time(0), rospy.Duration(10))
+        # listener = tf.TransformListener()
+        # listener.waitForTransform('robot_0_ns/map', 'robot_0/base_footprint', rospy.Time(0), rospy.Duration(10))
+        #
+        # try:
+        #     (trans, rot) = listener.lookupTransform("/map", robot_name + "/base_footprint", rospy.Time(0))
+        #     starting_location = (int(trans[0]), int(trans[1]))
+        #     rospy.loginfo("++++++++++++++++++++++++++++++++++++++++++++current position (%f,%f)" % (starting_location[0], starting_location[1]))
+        # except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException), e:
+        #     rospy.logerr("Service call failed: %s" % e)
 
-        try:
-            (trans, rot) = listener.lookupTransform("/map", robot_name + "/base_footprint", rospy.Time(0))
-            starting_location = (int(trans[0]), int(trans[1]))
-            rospy.loginfo("current position (%f,%f)" % (starting_location[0], starting_location[1]))
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException), e:
-            rospy.logerr("Service call failed: %s" % e)
 
-        # compute robot initial position in grid
-        starting_location_grid = (int(math.floor(
-            (starting_location[1] - response.map.info.origin.position.y) / robot_size)),
-                                  int(math.floor(
-                                      (starting_location[0] - response.map.info.origin.position.x) / robot_size))
-        )
+
+
+        # # compute robot initial position in grid
+        # starting_location_grid = (int(math.floor(
+        #     (starting_location[1] - response.map.info.origin.position.y) / robot_size)),
+        #                           int(math.floor(
+        #                               (starting_location[0] - response.map.info.origin.position.x) / robot_size))
+        # )
+
+        starting_location_grid = (len(grid)/2, len(grid[0])/2)
         print "starting location grid: " + str(starting_location_grid)
 
         # switch to coarse grid cell, each cell of size 4D
@@ -527,54 +483,57 @@ def main():
 
         # get coverage path in the fine grid. using the mst of the coarse grid
         path = get_coverage_path_from_mst(coarse_grid_mst, starting_location_grid)
-        # for p in path: print p
 
-        # # use send_goals code, create mini-plan for each move, then moving the robot through that plan
-        # last_p = path[0]
-        # if last_p.GoUp() == path[1]:
-        #     last_d = 'N'
-        # elif last_p.GoRight() == path[1]:
-        #     last_d = 'E'
-        # elif last_p.GoDown() == path[1]:
-        #     last_d = 'S'
-        # else:
-        #     last_d = 'W'
-        #
-        # begin_time = rospy.Time.now()
-        # covered_cells_count = 0
+        # use send_goals code, create mini-plan for each move, then moving the robot through that plan
+        last_p = path[0]
+        if last_p.GoUp() == path[1]:
+            last_d = 'N'
+        elif last_p.GoRight() == path[1]:
+            last_d = 'E'
+        elif last_p.GoDown() == path[1]:
+            last_d = 'S'
+        else:
+            last_d = 'W'
+
+        begin_time = rospy.Time.now()
+        covered_cells_count = 0
 
 
-        # # move the robot along the coverage path
-        # for p in path:
-        #     # Check if going along the same path as before, and stop only when changing direction, then call send_goals
-        #     if (last_d == 'N' and last_p.GoUp() == p) or \
-        #             (last_d == 'E' and last_p.GoRight() == p) or \
-        #             (last_d == 'S' and last_p.GoDown() == p) or \
-        #             (last_d == 'W' and last_p.GoLeft() == p):
-        #         last_p = p
-        #         covered_cells_count += 1
-        #         print "continue..."
-        #         continue
-        #     else:
-        #         if last_p.GoUp() == p:
-        #             last_d = 'N'
-        #         elif last_p.GoRight() == p:
-        #             last_d = 'E'
-        #         elif last_p.GoDown() == p:
-        #             last_d = 'S'
-        #         else:
-        #             last_d = 'W'
-        #
-        #         succeeded = send_goals(
-        #             int(last_p.col * robot_size + response.map.info.origin.position.x),
-        #             int(last_p.row * robot_size + response.map.info.origin.position.y))
-        #
-        #         if succeeded:
-        #             covered_cells_count += 1
-        #
-        #         last_p = p
-        #
-        # finish_time = rospy.Time.now()
+        # move the robot along the coverage path
+        for p in path:
+            # Check if going along the same path as before, and stop only when changing direction, then call send_goals
+            # if (last_d == 'N' and last_p.GoUp() == p) or \
+            #         (last_d == 'E' and last_p.GoRight() == p) or \
+            #         (last_d == 'S' and last_p.GoDown() == p) or \
+            #         (last_d == 'W' and last_p.GoLeft() == p):
+            #     last_p = p
+            #     covered_cells_count += 1
+            #     print "continue..."
+            #     continue
+            # else:
+                new_d = ""
+                if last_p.GoUp() == p:
+                    new_d = 'N'
+                elif last_p.GoRight() == p:
+                    new_d = 'E'
+                elif last_p.GoDown() == p:
+                    new_d = 'S'
+                else:
+                    new_d = 'W'
+
+                # print "%s -- X: %d, Y: %d" % (robot_name, int(last_p.col * robot_size + response.map.info.origin.position.x),
+                #                                 int(last_p.row * robot_size + response.map.info.origin.position.y))
+
+                succeeded = move_blindly(last_d, new_d, robot_size)
+                last_d = new_d
+
+                # succeeded = send_goals(
+                #     int(last_p.col * robot_size + response.map.info.origin.position.x),
+                #     int(last_p.row * robot_size + response.map.info.origin.position.y))
+
+                last_p = p
+
+        finish_time = rospy.Time.now()
         #
         # # print statistics of this problem
         # with open("/home/moshe/catkin_ws/src/coverage_4/src/coverage_stat.txt", "w+") as stats_file:
