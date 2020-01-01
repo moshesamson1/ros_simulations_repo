fa1626709ebc8e425550b67466044677a2754df4
moshesamson1 <samson.moshe@gmail.com>
2020-01-01 21:16:16 +0200

    take map correctly, reformat the world launch file and disable redundant nodes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='comp_cov_sim',
+    version='',
+    packages=['tqdm'],
+    url='',
+    license='',
+    author='moshe',
+    author_email='',
+    description=''
+)
