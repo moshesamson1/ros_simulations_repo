0216c1b3a21ee0ab5de5ab7a45e9aea71512b309
moshesamson1 <samson.moshe@gmail.com>
2020-02-03 21:11:26 +0200

    set different name to steps-time file according to env variables

--- src/logs.py
+++ src/logs.py
@@ -1,13 +1,16 @@
 from globals import Globals
+import os
 
 positions = []
 
 
 def logAllSteps():
-    positions_file = open(Globals.absolute_path + "/%s_positions" % Globals.robot_name, "a+")
+    gazebo_port = str(os.environ['GAZEBO_MASTER_URI']).split(':')[-1]
+    ros_port = str(os.environ['ROS_MASTER_URI']).split(':')[-1]
+    positions_file = open("%s/%s_%s_%s_positions" %(Globals.absolute_path, ros_port, gazebo_port, Globals.robot_name), "a+")
     positions_file.writelines(positions)
     positions_file.close()
 
 
 def logStep(slotPosition, coverageTime):
-    positions.append("slot:%s---time:%s\n" % (slotPosition, coverageTime))
+    positions.append("%s:%s\n" % (slotPosition, coverageTime))
